78eeeb4cb47e5696ecb130336fff1873a22a94cd
Ignacio <88597330+0ff5@users.noreply.github.com>
2023-04-30 20:01:43 -0300

    Update UnG.py

--- UnG.py
+++ UnG.py
@@ -1,22 +1,24 @@
+import concurrent.futures
 import requests
 from bs4 import BeautifulSoup
 from colorama import init, Fore
 
 init()
 
-# Function to check if a game is uncopylocked
 def is_uncopylocked(game_url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
     }
 
-    response = requests.get(game_url, headers=headers)
-
-    if response.status_code != 200:
-        print(Fore.RED + f"[ERROR] Failed to get game info for game url {game_url}: {response.text}")
+    try:
+        with requests.Session() as session:
+            response = session.get(game_url, headers=headers)
+            response.raise_for_status() # Raise an exception for any non-200 status codes
+    except requests.exceptions.RequestException as e:
+        print(Fore.RED + f"[ERROR] Failed to get game info for game url {game_url}: {e}")
         return False
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.content, 'lxml')
     game_context_menu_div = soup.find('div', {'id': 'game-context-menu'})
     if game_context_menu_div:
         print(Fore.GREEN + f"[SUCCESS] Found an uncopylocked game at: {game_url}")
@@ -32,27 +34,29 @@ def is_uncopylocked(game_url):
         print(Fore.YELLOW + f"[INFO] Game {game_url} is not uncopylocked")
         return False
 
-# Function to search for games with a keyword
-def search_games(keyword):
-    url = f'https://www.roblox.com/games/list-json?keyword={keyword}&startRows=0&maxRows=150'
+def search_uncopylocked(keyword, start_row, max_rows):
+    url = f'https://www.roblox.com/games/list-json?keyword={keyword}&startRows={start_row}&maxRows={max_rows}'
 
+    try:
+        response = requests.get(url)
 
-    response = requests.get(url)
+        if response.status_code != 200:
+            print(Fore.RED + f"[ERROR] Failed to search for games: {response.text}")
+            return []
 
-    if response.status_code != 200:
-        print(Fore.RED + f"[ERROR] Failed to search for games: {response.text}")
+        response_json = response.json()
+        game_urls = [game['GameDetailReferralUrl'] for game in response_json]
+
+        for game_url in game_urls:
+            is_uncopylocked(game_url)
+    except Exception as e:
+        print(Fore.RED + f"[ERROR] Failed to search for games: {e}")
         return []
 
-    response_json = response.json()
-    game_urls = [game['GameDetailReferralUrl'] for game in response_json]
+keyword = input("Enter a keyword to search for uncopylocked games (type 'exit' to quit): ")
+while keyword != 'exit':
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        for start_row in range(0, 9800, 200):
+            executor.submit(search_uncopylocked, keyword, start_row, 200)
 
-    return game_urls
-
-while True:
     keyword = input("Enter a keyword to search for uncopylocked games (type 'exit' to quit): ")
-    if keyword == 'exit':
-        break
-
-    game_urls = search_games(keyword)
-    for game_url in game_urls:
-        is_uncopylocked(game_url)
